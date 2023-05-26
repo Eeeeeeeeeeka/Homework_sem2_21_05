8015015958456aae1a2d264efcbe010fc33358c1
Elena_Kalegina <ea.kalegina@yandex.ru>
2023-05-26 22:03:20 +0500

    task 15 done

--- Task 15/Program.cs
+++ Task 15/Program.cs
@@ -3,3 +3,16 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.Clear();
+Console.Write("Введите номер дня недели: ");
+int n = int.Parse(Console.ReadLine()!);
+while (n < 1 || n > 7)
+{
+    Console.Write("Вы ошиблись!\nВведите номер дня недели: ");
+    n = int.Parse(Console.ReadLine()!);
+}
+if (n < 6)
+Console.Write("Сегодня не выходной :(((");
+else 
+Console.Write("Сегодня выходной!");
